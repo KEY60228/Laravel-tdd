9a00ecae9176ceb186d54ad2eb964875ef96f878
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-08-01 22:17:14 +0900

    1-4. Exercise (GREEN) (Model Answer)

--- laravel/tests/Feature/Http/Controllers/LessonControllerTest.php
+++ laravel/tests/Feature/Http/Controllers/LessonControllerTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature\Http\Controllers;
 
 use App\Models\Lesson;
 use App\Models\User;
+use App\Models\Reservation;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Http\Response;
@@ -24,13 +25,13 @@ class LessonControllerTest extends TestCase
     $lesson = factory(Lesson::class)->create(['name' => '楽しいヨガレッスン', 'capacity' => $capacity]);
     for ($i = 0; $i < $reservationCount; $i++) {
       $user = factory(User::class)->create();
-      $lesson->reservations()->save(factory(Reservation::class))->make(['user_id' => $user]);
+      $lesson->reservations()->save(factory(Reservation::class)->make(['user_id' => $user->id]));
     }
     $response = $this->get("/lessons/{$lesson->id}");
 
     $response->assertStatus(Response::HTTP_OK);
     $response->assertSee($lesson->name);
-    $response->assertSee('空き状況: $expectedVacancyLevelMark');
+    $response->assertSee("空き状況: {$expectedVacancyLevelMark}");
   }
 
   public function dataShow()
