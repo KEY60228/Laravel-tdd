1c6059ed55de5bab5ad2df8a6a0be0b42761c224
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-08-01 21:39:01 +0900

    1-4. Exercise (RED)

--- laravel/tests/Feature/Http/Controllers/LessonControllerTest.php
+++ laravel/tests/Feature/Http/Controllers/LessonControllerTest.php
@@ -12,13 +12,19 @@ class LessonControllerTest extends TestCase
 {
   use RefreshDatabase;
 
+
   public function testShow()
   {
-    $lesson = factory(Lesson::class)->create(['name' => '楽しいヨガレッスン']);
+    $lesson = factory(Lesson::class)->create(['name' => '楽しいヨガレッスン', 'capacity' => $capacity]);
+    for ($i = 0; $i < $reservationCount; $i++) {
+      $user = factory(User::class)->create();
+      $lesson->reservations()->save(factory(Reservation::class))->make(['user_id' => $user]);
+    }
     $response = $this->get("/lessons/{$lesson->id}");
 
     $response->assertStatus(Response::HTTP_OK);
     $response->assertSee($lesson->name);
-    $response->assertSee('空き状況: ×');
+    $response->assertSee('空き状況: $expectedVacancyLevelMark');
   }
+
 }
