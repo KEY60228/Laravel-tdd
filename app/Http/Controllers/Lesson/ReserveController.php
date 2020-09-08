e733e068a74e4344a0a0c3e00c334b5be9693fa1
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-09-08 22:44:13 +0900

    3-4. GREEN

--- app/Http/Controllers/Lesson/ReserveController.php
+++ app/Http/Controllers/Lesson/ReserveController.php
@@ -5,12 +5,15 @@ namespace App\Http\Controllers\Lesson;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Lesson;
+use App\Models\Reservation;
+use Illuminate\Support\Facades\Auth;
 
 class ReserveController extends Controller
 {
     public function __invoke(Lesson $lesson)
     {
-      // ToDo: 予約
+      $user = Auth::user();
+      Reservation::create(['lesson_id' => $lesson->id, 'user_id' => $user->id]);
 
       return redirect()->route('lessons.show', ['lesson' => $lesson]);
     }
